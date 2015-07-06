ac815a31e53c92095f4f6d8f71d07896c0883a69
André Gonçalves <andreslb1@gmail.com>
2015-07-06 16:09:35 +0200

    Reindex as salaried

--- src/bda/plone/shop/checkout.py
+++ src/bda/plone/shop/checkout.py
@@ -10,6 +10,7 @@ from zope.interface import Interface
 from bda.plone.orders import interfaces as ifaces
 import transaction
 
+from bda.plone.orders.common import get_orders_soup
 
 @implementer(ICheckoutSettings)
 @adapter(Interface)
@@ -21,10 +22,15 @@ class CheckoutSettings(object):
     def skip_payment(self, uid):
         settings = get_shop_payment_settings()
         order_data = OrderData(self.context, uid=uid)
+        
         # order total is 0, skip
         if not Decimal(str(order_data.total)).quantize(Decimal('1.000')):
+            orders_soup = get_orders_soup(self.context)
             order_data.order.attrs['salaried'] = ifaces.SALARIED_YES
+            order_data.salaried = ifaces.SALARIED_YES
+            orders_soup.reindex(records=[order_data.order])
             return True
+        
         # if payment should be skipped if order contains reservations and
         # order contains reservations, skip
         if settings.skip_payment_if_order_contains_reservations:
